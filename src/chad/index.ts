d10987cd679b6b1bc1b553c836b6057268b28b1a
Sergio Sansovini <redsanso@gmail.com>
2024-12-12 23:44:30 +0100

    simple hello.js file generation

--- src/chad/index.ts
+++ src/chad/index.ts
@@ -1,10 +1,16 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Schema } from './schema';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function chad(_options: any): Rule {
+export function chad(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    let name = _options.name;
+    if(tree.exists('hello.js')){
+      tree.delete('hello.js');
+  }
+  tree.create('hello.js', `console.log('Hello ${name}')`);
+  return tree;
   };
 }
