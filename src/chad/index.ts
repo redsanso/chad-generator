15d8bd74d467828de591000cd7169880fc1394f2
Sergio Sansovini <redsanso@gmail.com>
2024-12-12 23:51:10 +0100

    working templating example

--- src/chad/index.ts
+++ src/chad/index.ts
@@ -1,16 +1,27 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { Schema } from './schema';
-
+import { strings } from "@angular-devkit/core";
+import {
+  apply,
+  mergeWith,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url,
+} from "@angular-devkit/schematics";
+import { Schema } from "./schema";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function chad(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    let name = _options.name;
-    if(tree.exists('hello.js')){
-      tree.delete('hello.js');
-  }
-  tree.create('hello.js', `console.log('Hello ${name}')`);
-  return tree;
+    const sourceTemplate = url("./files");
+    const parameterizedTemplate = apply(sourceTemplate, [
+      template({
+        ..._options,
+        ...strings,
+      }),
+    ]);
+    tree = mergeWith(parameterizedTemplate)(tree, _context) as Tree;
+    return tree;
   };
 }
