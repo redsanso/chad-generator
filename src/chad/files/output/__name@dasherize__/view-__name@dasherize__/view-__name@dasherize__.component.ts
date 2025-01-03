c21f13063f93fe490263dd3007eef771171c652c
Sergio Sansovini <redsanso@gmail.com>
2025-01-03 18:29:54 +0100

    id on view template and improvements

--- src/chad/files/output/__name@dasherize__/view-__name@dasherize__/view-__name@dasherize__.component.ts
+++ src/chad/files/output/__name@dasherize__/view-__name@dasherize__/view-__name@dasherize__.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule, RouterModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, RouterModule } from "@angular/router";
 import { Observable } from "rxjs";
 import { <%= classify(name) %> } from '../<%= dasherize(name) %>';
 import { <%= classify(name) %>ApiService } from '../<%= dasherize(name) %>.service';
@@ -7,19 +8,23 @@ import { <%= classify(name) %>ApiService } from '../<%= dasherize(name) %>.servi
 @Component({
     selector: "view-<%= dasherize(name) %>",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
 	templateUrl: "./view-<%= dasherize(name) %>.component.html",
 	styleUrl: "./view-<%= dasherize(name) %>.component.css",
 })
 export class View<%= classify(name) %>Component implements OnInit {
 
+    id?: number;
     data$?: Observable<<%= classify(name) %>>;
 
     constructor(
-        private readonly service: <%= classify(name)%>ApiService
+        private readonly service: <%= classify(name)%>ApiService,
+		private readonly route: ActivatedRoute,
     ){}
 
     ngOnInit() {
-        this.data$ = this.service.findOne();
+        this.id = this.route.snapshot.params["id"] || "";
+
+        this.data$ = this.service.findOne(this.id);
     }
 }
