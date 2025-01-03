dc7ef72571d0a50c2fa4c0a38bc9b4129aed87cb
Sergio Sansovini <redsanso@gmail.com>
2025-01-03 18:24:48 +0100

    typo fix

--- src/chad/files/output/__name@dasherize__/__name@dasherize__.routes.ts
+++ src/chad/files/output/__name@dasherize__/__name@dasherize__.routes.ts
@@ -1,21 +1,19 @@
 import { Routes } from "@angular/router";
 
 export const routes: Routes = [
-    [
-        {
-            path: "",
-            pathMatch: "full",
-            redirectTo: "search",
-        },
-        {
-            path: "search",
-            data: {},
-            loadComponent: () => import("./search-<%= dasherize(name) %>/search-<%= dasherize(name) %>.component").then(c => c.Search<%= classify(name) %>Component)
-        },
-        {
-            path: "view/:id",
-            data: {},
-            loadComponent: () => import("./view-<%= dasherize(name) %>/view-<%= dasherize(name) %>.component").then(c => c.View<%= classify(name) %>Component)
-        }
-    ]
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "search",
+    },
+    {
+        path: "search",
+        data: {},
+        loadComponent: () => import("./search-<%= dasherize(name) %>/search-<%= dasherize(name) %>.component").then(c => c.Search<%= classify(name) %>Component)
+    },
+    {
+        path: "view/:id",
+        data: {},
+        loadComponent: () => import("./view-<%= dasherize(name) %>/view-<%= dasherize(name) %>.component").then(c => c.View<%= classify(name) %>Component)
+    }
 ];
